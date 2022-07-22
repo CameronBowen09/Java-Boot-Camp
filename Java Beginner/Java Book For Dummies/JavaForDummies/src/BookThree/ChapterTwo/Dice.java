3693ec371edcd47dd77604f09e7548508340f597
CameronBowen09 <cameronbowen075@gmail.com>
2022-07-22 08:52:15 +0200

    Added Chapter Two

---
+++ Java Beginner/Java Book For Dummies/JavaForDummies/src/BookThree/ChapterTwo/Dice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BookThree.ChapterTwo;
+
+/**
+ *
+ * @author User
+ */
+public class Dice {
+
+    public void roll() {
+        // code that rolls the dice goes here
+    }
+}
