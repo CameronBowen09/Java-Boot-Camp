1d765639861f39f3eb62adcf0c1583fef10176ba
CameronBowen09 <cameronbowen075@gmail.com>
2022-08-12 14:12:31 +0200

    Added Classwork Chapter 16

--- Java Advanced/labs2/16-ForkJoin/practices/practice1/ForkJoinFindMax16-01Prac/src/com/example/Main.java
+++ Java Advanced/labs2/16-ForkJoin/practices/practice1/ForkJoinFindMax16-01Prac/src/com/example/Main.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -10,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
         int[] data = new int[1024 * 1024 * 128]; //512MB
 
-        for (int i = 0; i < data.length; i++) {
-            data[i] = ThreadLocalRandom.current().nextInt();
-        }
+//        for (int i = 0; i < data.length; i++) {
+//            data[i] = ThreadLocalRandom.current().nextInt();
+//        }
 
 //        int max = Integer.MIN_VALUE;
 //        for (int value : data) {
@@ -23,6 +22,10 @@ public class Main {
 //        System.out.println("Max value found:" + max);
         
         ForkJoinPool pool = new ForkJoinPool();
+        
+        RandomArrayAction action = new RandomArrayAction(data, 0, data.length-1, data.length/16);
+        pool.invoke(action);
+        
         FindMaxTask task = new FindMaxTask(data, 0, data.length-1, data.length/16);
         Integer result = pool.invoke(task);
         System.out.println("Max value found:" + result);
