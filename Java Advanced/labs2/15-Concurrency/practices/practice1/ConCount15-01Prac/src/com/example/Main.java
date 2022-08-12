7b0f40855d6585ca85680b762428945e895eb510
CameronBowen09 <cameronbowen075@gmail.com>
2022-08-12 14:07:08 +0200

    Added Classwork Chapter 15

--- Java Advanced/labs2/15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/Main.java
+++ Java Advanced/labs2/15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/Main.java
@@ -7,10 +7,14 @@ public class Main {
 
   public static void main(String[] args) {
     // Setup Executor
+    ExecutorService es = Executors.newCachedThreadPool();
     
     // Submit Runnable
+    es.submit(new CountRunnable(20,"A"));
+    es.submit(new CountRunnable(20,"B"));
+    es.submit(new CountRunnable(20,"C"));
     
     // Showndown ExectuorService
-
+    es.shutdown();
   }
 }
