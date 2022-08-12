dc87c12368508ec78288e6037fe55b94e910822c
CameronBowen09 <cameronbowen075@gmail.com>
2022-08-12 13:34:03 +0200

    Added Classwork Chapter 11

--- Java Advanced/labs2/11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeImpl.java
+++ Java Advanced/labs2/11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeImpl.java
@@ -12,18 +12,42 @@ public class EmployeeImpl {
     EmployeeImpl() {
     }
 
-    public void add(Employee emp) {
-
+    // Add an Employee record
+    public void add(Employee emp) throws InvalidOperationException {
+        if (employeeArray[emp.getId()] != null) {
+            throw new InvalidOperationException("Error adding employee , employee id already exists " + emp.getId());
+        }
+        try {
+            employeeArray[emp.getId()] = emp;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error adding employee , id must be less than " + employeeArray.length);
+        }
     }
 
-    public void delete(int id) {
-
+    // Delete an employee record that has this ID
+    public void delete(int id) throws InvalidOperationException {
+        if (employeeArray[id] == null) {
+            throw new InvalidOperationException("Error deleting employee, no such employee " + id);
+        }
+        try {
+            employeeArray[id] = null;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error deleting employee, id must be less than " + employeeArray.length);
+        }
     }
 
-    public Employee findById(int id) 
-    {
-        return employeeArray[id];
+    // Find an Employee record using this ID
+    public Employee findById(int id) throws InvalidOperationException {
+        try {
+            return employeeArray[id];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error finding employee ", e);
+        }
     }
+
+    // Return an array of all of the Employee records
+    // We are using a collection List object to store the results
+    // This makes it easier to just add to the collection
     public Employee[] getAllEmployees() {
         List<Employee> emps = new ArrayList<>();
         // Iterate through the memory array and find Employee objects
--- Java Advanced/labs2/11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeTest.java
+++ Java Advanced/labs2/11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeTest.java
@@ -2,24 +2,31 @@ package com.example;
 
 public class EmployeeTest {
     
-    public static void main(String[] args)  {
-        Employee e1 = new Employee(0, "bob", "builder", 1250.50f);
-        EmployeeImpl emp = new EmployeeImpl();
-        emp.add(e1);
-        Employee e2 = new Employee(1, "harry", "potter", 1250.50f);
-        emp.add(e2);
-        Employee e3 = new Employee(5, "harry", "potter", 1250.50f);
-        emp.add(e3);
-        Employee[] allEmps = emp.getAllEmployees();
-        for (Employee employee : allEmps) {
-            System.out.println(employee + "\n");
-        }
+    public static void main(String[] args) throws InvalidOperationException {
+      EmployeeImpl empArr = new EmployeeImpl();
+      
+      Employee e1 = new Employee(0, "bob", "builder", 1250.50f);
+      empArr.add(e1);
+      
+      Employee e2 = new Employee(1, "John", "Doe", 1250.50f);
+      empArr.add(e2);
+      
+      Employee e3 = new Employee(5, "Jane", "Doe", 1350.50f);
+      empArr.add(e3);
+      
+      Employee[] allEmps = empArr.getAllEmployees();
+      for (Employee employee : allEmps) {
+          System.out.println(employee + "\n");
+      }
         
-      //emp.delete(7);
-       
-        
-                }          
+      
+      Employee e = empArr.findById(5);
+      if (e != null) {
+          System.out.println(empArr + "\n");
+      } else {
+          System.out.println("\n\nEmployee  not found");     
+      }          
     
-    
-    
+      //empArr.delete(7);
     }
+}
